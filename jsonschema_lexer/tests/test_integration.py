302d7154b35e3ee8b86060483aa7b0a23031a0e8
jarvis <dhruv1778@gmail.com>
2024-03-02 18:42:38 +0530

    Add tests for all dialects

--- jsonschema_lexer/tests/test_integration.py
+++ jsonschema_lexer/tests/test_integration.py
@@ -7,20 +7,21 @@ import pytest
 
 from jsonschema_lexer import JSONSchemaLexer
 
+dialects = [
+    "http://json-schema.org/draft-01/schema#",
+    "http://json-schema.org/draft-02/schema#",
+    "http://json-schema.org/draft-03/schema#",
+    "http://json-schema.org/draft-04/schema#",
+    "http://json-schema.org/draft-06/schema#",
+    "http://json-schema.org/draft-07/schema#",
+    "https://json-schema.org/draft/2019-09/schema",
+    "https://json-schema.org/draft/2020-12/schema",
+]
+
 
 @pytest.fixture()
 def lexer():
     return JSONSchemaLexer()
-
-
-@pytest.fixture()
-def data_types():
-    return JSONSchemaLexer().parsed_data_types
-
-
-@pytest.fixture()
-def keywords():
-    return JSONSchemaLexer().parsed_keywords
 
 
 def assert_single_token(lexer, s, token):
@@ -42,31 +43,427 @@ def assert_tokens(lexer, string, expected_tokens):
     assert parsed_tokens == expected_tokens
 
 
-def test_data_type_tokens(lexer, data_types):
-    for data_type in data_types:
+def test_data_type_tokens(lexer):
+    for data_type in lexer.data_types:
         assert_single_token(lexer, data_type, Token.Name.Decorator)
 
 
-def test_keyword_tokens(lexer, keywords):
-    for keyword in keywords:
-        sample_json_schema = f"""
-        {{
-            {keyword}:"test"
-        }}
-        """.strip()
-        assert_tokens(
-            lexer,
-            sample_json_schema,
-            [
-                Token.Punctuation,
-                Token.Text.Whitespace,
-                Token.Keyword,
-                Token.Punctuation,
-                Token.Literal.String.Double,
-                Token.Text.Whitespace,
-                Token.Punctuation,
-            ],
-        )
+def test_default_schema_support():
+    for dialect in dialects:
+        dialect = f'"{dialect}"'
+        lexer = JSONSchemaLexer(dialect)
+        assert lexer.default_dialect == dialect
+
+
+def test_keyword_tokens():
+    for dialect in dialects:
+        lexer = JSONSchemaLexer(dialect)
+        dialect = f'"{dialect}"'
+        for keyword in lexer.keywords[dialect]:
+            if keyword == "$schema":
+                continue
+            sample_json_schema = f"""
+            {{
+                "{keyword}":"test"
+            }}
+            """.strip()
+            assert_tokens(
+                lexer,
+                sample_json_schema,
+                [
+                    Token.Punctuation,
+                    Token.Text.Whitespace,
+                    Token.Keyword,
+                    Token.Punctuation,
+                    Token.Literal.String.Double,
+                    Token.Text.Whitespace,
+                    Token.Punctuation,
+                ],
+            )
+
+
+def test_no_schema_with_default():
+    dialect = '"https://json-schema.org/draft/2020-12/schema"'
+    lexer = JSONSchemaLexer(dialect)
+    test_json_schema = """
+    {
+    "prefixItems": [true], // <- a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_no_schema_without_default(lexer):
+    test_json_schema = """
+    {
+    "prefixItems": [true], // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft202012_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "prefixItems": [true], // <- a keyword
+    "additionalItems": false, // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft201909_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "https://json-schema.org/draft/2019-09/schema",
+    "$recursiveRef": "test", // <- a keyword
+    "$dynamicRef": "test", // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft07_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "prefixItems": [true], // <- not a keyword
+    "additionalItems": false, // <- a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword.Constant,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft06_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "const": "test", // <- a keyword
+    "propertyNames": "test", // <- a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft04_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "multipleOf": "test", // <- a keyword
+    "divisibleBy": "test", // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft03_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-03/schema#",
+    "patternProperties": "test", // <- a keyword
+    "optional": "test", // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft02_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-02/schema#",
+    "id": "test", // <- a keyword
+    "uniqueItems": "test", // <- a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
+
+
+def test_draft01_schema(lexer):
+    test_json_schema = """
+    {
+    "$schema": "http://json-schema.org/draft-01/schema#",
+    "id": "test", // <- a keyword
+    "uniqueItems": "test", // <- not a keyword
+    }
+    """.strip()
+    assert_tokens(
+        lexer,
+        test_json_schema,
+        [
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Keyword,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Name.Tag,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Literal.String.Double,
+            Token.Punctuation,
+            Token.Text.Whitespace,
+            Token.Comment.Single,
+            Token.Text.Whitespace,
+            Token.Punctuation,
+        ],
+    )
 
 
 def test_nested_json_schema(lexer):
